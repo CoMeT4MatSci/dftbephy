aa1d7b6026b5b3c46b2625e00c97f76a239bcc69
Alexander Croy <alexcroy@gmail.com>
2025-08-22 14:38:57 +0200

    Upgrade package setup using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages, Extension
 import numpy
 
-ext = Extension(name='dftbephy.extensions', sources=['dftbephy/extensions.pyx'],
-                language='c++',
-                include_dirs=[numpy.get_include()])
+setup_args = dict(
+    packages=find_packages(
+        where='dftbephy',  # '.' by default
+    ),
+    ext_modules = [Extension(name='dftbephy.extensions', sources=['dftbephy/extensions.pyx'],
+                    language='c++',
+                    include_dirs=[numpy.get_include()])]
+    )
 
-setup(
-    name='dftbephy',
-    version='0.0.1',
-    packages=find_packages(),
-    ext_modules=[ext],
-    install_requires=['numpy', 'scipy', 'phonopy']
-)
+setup(**setup_args)
+
