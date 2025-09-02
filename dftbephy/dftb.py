da5c7a05332af1d8b4a9d1a9c35b9a4c936a158f
Alexander Croy <alexcroy@gmail.com>
2025-09-02 13:44:37 +0200

    Support SCC in reference and gradient calculations.

--- dftbephy/dftb.py
+++ dftbephy/dftb.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import re
 import numpy as np
+import string
 
 from .units import *
 from .fileio import writegen 
@@ -11,18 +12,55 @@ ENERGY_PATTERN = re.compile(r"mermin_energy[^:]*:[^:]*:[^:]*:\s*(?P<value>\S+)")
 # this is the standard order of orbitals as stated in the dftb+ manual
 std_orbital_order = {'s': ['s'], 'p': ['py', 'pz', 'px'], 'd': ['dxy', 'dyz', 'dz2', 'dxz', 'dx2-y2']}
 
-def calculate_reference(binary, reffile, coords, specienames, species, origin,
-                        latvecs):
-    """Calculates reference system
-       unmodified from calcderivs.py (DFTB+ developers group)
+def run_dftb(binary, reffile=None):
+    """Run dftb calculations using the given binary.
     """
-
-    writegen("geo.gen", (specienames, species, coords, origin, latvecs))
     with open('out.txt', "w") as outfile:
         with open('err.txt', "w") as errfile:
             subprocess.run([binary], stdout=outfile, stderr=errfile)
-    os.rename("autotest.tag", reffile)
+    if reffile:
+        os.rename("autotest.tag", reffile)
+    
 
+def calculate_reference(binary, reffile, coords, specienames, species, origin,
+                        latvecs, scc=False):
+    """Calculates reference system
+       adapted from calcderivs.py (DFTB+ developers group)
+    """
+
+    # write input geometry
+    writegen("geo.gen", (specienames, species, coords, origin, latvecs))
+
+    if scc:
+        # read template file
+        with open('dftb_in.template') as t:
+            template = string.Template(t.read())
+
+        # setup for scc calculation
+        final_output = template.substitute(maxscciterations=100, 
+                                           readinitialcharges='No', 
+                                           writehs='No')
+        # write file
+        with open('dftb_in.hsd', 'w') as output:
+            output.write(final_output)
+
+        # run dftb+
+        run_dftb(binary, reffile)
+    
+        # read template file
+        with open('dftb_in.template') as t:
+            template = string.Template(t.read())
+
+        # setup for scc calculation
+        final_output = template.substitute(maxscciterations=1, 
+                                           readinitialcharges='Yes', 
+                                           writehs='Yes')
+        # write file
+        with open('dftb_in.hsd', 'w') as output:
+            output.write(final_output)
+            
+    # run dftb+
+    run_dftb(binary, reffile)
 
 def calculate_forces(binary, disp, coords, specienames, species, origin,
                      latvecs):
@@ -40,9 +78,7 @@ def calculate_forces(binary, disp, coords, specienames, species, origin,
                 writegen("geo.gen", (specienames, species, newcoords, origin,
                                      latvecs))
 
-                with open('out.txt', "w") as outfile:
-                    with open('err.txt', "w") as errfile:
-                        subprocess.run([binary], stdout=outfile, stderr=errfile)
+                run_dftb(binary)
 
                 fp = open("autotest.tag", "r")
                 txt = fp.read()
@@ -57,59 +93,8 @@ def calculate_forces(binary, disp, coords, specienames, species, origin,
             forces[iat][ii] = (energy[0] - energy[1]) / (2.0 * disp)
     return forces
 
-def calculate_hamiltonian_derivs(binary, disp, coords, specienames, species, origin,
-                     latvecs):
-    """Calculates derivatives of hamiltonian by finite differences. ALL atoms are displaced by disp in ALL directions.
-       Only the first k-point is currently used!
-       based on calculate_forces from calcderivs.py (DFTB+ developers group)
-    """
-
-    with open('hamsqr1.dat') as f:
-        first_line = f.readline()
-        second_line = f.readline()
-
-    temp_dat = second_line.split()
-
-    real_elem = temp_dat[0] == 'T'
-    NALLORB = int(temp_dat[1])
-    NKPOINT = int(temp_dat[2])
-
-    if real_elem:
-        skip = 1
-    else:
-        skip = 2
-    
-    hamiltonian = np.empty((2, NALLORB, NALLORB), dtype=float)
-    ham_derivs = np.empty((len(coords), 3, NALLORB, NALLORB), dtype=float)
-    for iat in range(len(coords)): # iterate over atoms
-    
-        for ii in range(3): # iterate over cartesian coordinates
-            
-            for jj in range(2): # forward-backward displacement
-                newcoords = np.array(coords)
-                newcoords[iat][ii] += float(2 * jj - 1) * disp 
-                writegen("geo.gen", (specienames, species, newcoords, origin,
-                                     latvecs))
-                # run dftb
-                with open('out.txt', "w") as outfile:
-                    with open('err.txt', "w") as errfile:
-                        subprocess.run([binary], stdout=outfile, stderr=errfile)
-                        
-                # read hamsqr file                
-                # HamSqr_real[0] H_0 (undistorted)
-                # HamSqr_real[n] H for each displacement n=1,2,..n
-
-                # TODO read all k-points
-                # for n in range (1,NKPOINT+1):
-                n = 1
-                hamiltonian[jj] = np.loadtxt('hamsqr1.dat', skiprows= 2 + n*3 + (n-1)*NALLORB , max_rows=NALLORB)[:,::skip]
-                    
-            ham_derivs[iat][ii] = (hamiltonian[1] - hamiltonian[0]) / (2.0 * disp)
-            
-    return ham_derivs*HARTREE__EV
-
 def calculate_hamiltonian_derivs(binary, disp, atom_ids, coords, specienames, species, origin,
-                     latvecs):
+                     latvecs, scc=False):
     """Calculates derivatives of hamiltonian and overlap matrix by finite differences. SELECTED atoms with atom_ids are displaced by disp in ALL directions.
        Only the first k-point is currently used!
        based on calculate_forces from calcderivs.py (DFTB+ developers group)
@@ -148,16 +133,39 @@ def calculate_hamiltonian_derivs(binary, disp, atom_ids, coords, specienames, sp
                 newcoords[iat][ii] += float(2 * jj - 1) * disp 
                 writegen("geo.gen", (specienames, species, newcoords, origin,
                                      latvecs))
-                                     
-                # run dftb
-                with open('out.txt', "w") as outfile:
-                    with open('err.txt', "w") as errfile:
-                        subprocess.run([binary], stdout=outfile, stderr=errfile)
-                
-                # read hamsqr file                
-                # HamSqr_real[0] H_0 (undistorted)
-                # HamSqr_real[n] H for each displacement n=1,2,..n
 
+                if scc:
+                    # read template file
+                    with open('dftb_in.template') as t:
+                        template = string.Template(t.read())
+
+                    # setup for scc calculation
+                    final_output = template.substitute(maxscciterations=100, 
+                                                       readinitialcharges='No', 
+                                                       writehs='No')
+                    # write file
+                    with open('dftb_in.hsd', 'w') as output:
+                        output.write(final_output)
+
+                    # run dftb+
+                    run_dftb(binary, reffile)
+
+                    # read template file
+                    with open('dftb_in.template') as t:
+                        template = string.Template(t.read())
+
+                    # setup for scc calculation
+                    final_output = template.substitute(maxscciterations=1, 
+                                                       readinitialcharges='Yes', 
+                                                       writehs='Yes')
+                    # write file
+                    with open('dftb_in.hsd', 'w') as output:
+                        output.write(final_output)
+
+                # run dftb+
+                run_dftb(binary)
+                                      
+                # read hamsqr file
                 # TODO read all k-points
                 # for n in range (1,NKPOINT+1):
                 n = 1
