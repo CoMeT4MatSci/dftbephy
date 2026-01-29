20cf0a8bebe010f18420355ec26c736cd07a8172
efunsal <83028058+efunsal@users.noreply.github.com>
2026-01-29 15:19:17 +0100

    Add Phonopy input handling
    Added support for reading Phonopy parameters (yaml_file, symprec) from the dftbephy_hsd.in input file.

--- scripts/dftbephy-mobility-mpi.py
+++ scripts/dftbephy-mobility-mpi.py
@@ -96,6 +96,11 @@ if len(angular_momenta) == 0:
         print('-- angular momenta per element have to be specified.')
     quit()
 
+# read section for Phonopy
+phonopy_dict = inp_dict.get('Phonopy', {})
+phonopy_yaml    = phonopy_dict.get('yaml_file', 'phonopy_disp.yaml')
+phonopy_symprec = phonopy_dict.get('symprec', None)
+
 # read section for conductivity calculations
 mob_dict = check_hsd_input(inp_dict, 'Conductivities')
 
@@ -166,7 +171,13 @@ assert(type(spin_factor) is int)
 if rank == 0:
     print('-- looking for phonopy results in %s' % (basedir + phonopy_dir))
 os.chdir(basedir + phonopy_dir)
-ph = phonopy.load('phonopy_disp.yaml')
+
+if phonopy_symprec is None:
+    ph = phonopy.load(phonopy_yaml)
+    print("-- using Phonopy's default symmetry precision.")
+else:
+    ph = phonopy.load(phonopy_yaml, symprec=phonopy_symprec)
+    print("-- symmetry precision set to", phonopy_symprec)
 
 ###############################################################################
 # 2 Initialize DFTB calculator
